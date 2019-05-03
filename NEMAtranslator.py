9445dd20e424a81a3fe982e9d81039f2cce75cc0
Erin Teevens <45052152+eteevens@users.noreply.github.com>
2019-05-03 11:12:45 -0400

    Updated string manipulation method
    This simplified some of the code.

--- NEMAtranslator.py
+++ NEMAtranslator.py
@@ -14,39 +14,37 @@ recieving = recievable(0) #the actual recieving value
 def gpsRecieve(n):
     #the function that checks whether we are recieving actual gps data
     global recieving #sets up the function to work with the globabl variable
-    if data[n][0:6] == '$GPRMC':
-        if data[n][9] == 'V':
+    if '$GPRMC' in data[n]:
+        if data[n].split(',')[2] == 'V':
             recieving = recievable(1)
-        if data[n][18] == 'A':
+        if data[n].split(',')[2] == 'A':
             recieving = recievable(2)
     else:
         recieving = recievable(0)
     return recieving
 
-
 def gpsCoord(n):
     #the function that reads the GPS coordinates from the NEMA data and prints
     #as coordinates google maps can read
     #n is the placement of the data
-    if data[n][0:6] == '$GPGGA':
-        lat = [str(data[n][18:20]), str(data[n][20:27]), str(data[n][28])]
-        lon = [str(data[n][31:33]), str(data[n][33:40]), str(data[n][41])]
-        print(lat[0] + ' ' + lat[1] + ' ' + lat[2])
-        print(lon[0] + ' ' + lon[1] + ' ' + lon[2])
-    if data[n][0:6] == '$GPRMC':
-        lat = [str(data[n][20:22]), str(data[n][22:29]), str(data[n][30])]
-        lon = [str(data[n][33:35]), str(data[n][35:42]), str(data[n][43])]
-        print(lat[0] + ' ' + lat[1] + ' ' + lat[2])
-        print(lon[0] + ' ' + lon[1] + ' ' + lon[2])
+    if '$GPRMC' in data[n] or '$GPGGA' in data[n]:
+        wdata = data[n].split(',')
+        if '$GPRMC' in data[n]:
+            lat = [wdata[3][0:2], wdata[3][2:], wdata[4]]
+            lon = [wdata[5][0:2], wdata[5][2:], wdata[6]]
+        if '$GPGGA' in data[n]:
+            lat = [wdata[2][0:2], wdata[2][2:], wdata[3]]
+            lon = [wdata[4][0:2], wdata[4][2:], wdata[5]]
+        print(' '.join(lat))
+        print(' '.join(lon))
 
 def gpsTime(n):
     #the function that reads the time from the NEMA data and prints it as a UTC
     #timestamp
-    if data[n][0:6] == '$GPGGA' or data[n][0:6] == '$GPRMC':
-        hr = str(data[n][7:9])
-        min = str(data[n][9:11])
-        sec = str(data[n][11:13])
-        print(hr + ':' + min + ':' + sec)
+    if '$GPRMC' in data[n] or '$GPGGA' in data[n]:
+        wdata = data[n].split(',')[1]
+        wdata = [wdata[0:2], wdata[2:4], wdata[4:6]]
+        print(':'.join(wdata))
 
 gpsRecieve(0) #testing to see if we're recieving data
 gpsRecieve(1)
